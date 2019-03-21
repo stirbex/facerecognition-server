2337b176dd7be7a59c095052a1cf8fe1339d1484
stirbex <stirbexsrl@yahoo.com>
2019-03-21 20:39:45 +0200

    update

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
-var cors = require('cors')
+var cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'rapidistul14',
+    database : 'smart-brain'
+  }
+});
+
 
 const database = {
   users: [{
@@ -42,24 +54,56 @@ app.post('/findface', (req, res) => {
 
 
 app.post('/register', (req, res) => {
-  database.users.push({
-    id: '124',
-    name: req.body.name,
-    email: req.body.email,
-    entries: 0,
-    joined: new Date()
+  const {email, name, password} = req.body;
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx =>{
+    trx.insert({
+      hash: hash,
+      email: email,
+    })
+    .into("login")
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+        .returning('*')
+        .insert({
+        email: loginEmail,
+        name: name,
+        joined: new Date()
+        })
+        .then(user =>{
+          res.json(user[0]);
+        })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
   })
-  res.json(database.users[database.users.length - 1])
+  .catch(err => res.status(400).json("unable to register"))
 })
 
-app.get('/profile/:userId', (req, res) => {
-  database.users.forEach(user => {
-    if (user.id === req.params.userId) {
-      return res.json(user);
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({ 
+  id})
+  .then(user => { res.json(user[0])
+    if(user.length) {
+      res.json(user[0])
+    } else{
+      res.status(400).json('Not found')
     }
   })
-  // res.json('no user')
-
+  .catch(err => res.status(400).json("error getting user"))
 })
+
+app.put('/image',(req, res)=> {
+  const {id} = req.body;
+  db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.jsonp(entries[0]);
+    })
+    .catch(err => { res.status(400).json("unable to get entries")})
+} )
 
 app.listen(3010, () => console.log('Example app listening on port 3010!'))
